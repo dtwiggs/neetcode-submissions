4dd54f3a131230ea7b770cfdbdc07d5a23a83971
Dalton <125609502+dtwiggs@users.noreply.github.com>
2026-05-12 14:41:40 -0700

    Add: number-of-students-unable-to-eat-lunch - submission-0

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int circular = 0, square = 0;
+        for(int student : students){
+            if(student == 0){
+                circular++;
+            } else{
+                square++;
+            }
+        }
+
+        for(int sandwich : sandwiches){
+            if(sandwich == 0){
+                if(circular > 0){
+                    circular--;
+                } else{
+                    break;
+                }
+            } else{
+                if(square > 0){
+                    square--;
+                } else{
+                    break;
+                }
+            }
+        }
+        return circular + square;
+    }
+}
